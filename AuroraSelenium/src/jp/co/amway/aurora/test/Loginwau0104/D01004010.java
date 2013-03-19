3dd09aa4a75bb69e30e3cc5609f848bca553e286
AJ83617 <AJ83617@JPWSVPD7H-1.na.intranet.msd>
2013-03-19 03:48:55 -0430

    JUNIT TEST

--- AuroraSelenium/src/jp/co/amway/aurora/test/Loginwau0104/D01004010.java
+++ AuroraSelenium/src/jp/co/amway/aurora/test/Loginwau0104/D01004010.java
@@ -1,73 +1,54 @@
 package jp.co.amway.aurora.test.Loginwau0104;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
+import jp.co.amway.aurora.test.util.AuroraTestCase;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+public class D01004010 extends AuroraTestCase {
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-public class D01004010 extends TestCase {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://ipdev.amwaylive.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@Test
+	public void testD01004010() throws Exception {
+		driver.get(baseUrl
+				+ "aurora/source/ts/uc_common/view/WA-C-01-F012.html");
+		driver.findElement(
+				By.cssSelector("a.naviOpen.naviOpenBtn > div > span")).click();
+		driver.findElement(By.id("aMenuLogin")).click();
+		driver.findElement(By.linkText("パスワードを忘れた方")).click();
+		driver.findElement(By.id("txtIboNumber")).clear();
+		driver.findElement(By.id("txtIboNumber")).sendKeys("5243017");
+		driver.findElement(By.id("txtPin")).clear();
+		driver.findElement(By.id("txtPin")).sendKeys("1061");
+		driver.findElement(By.cssSelector("#btnSetPassword > div")).click();
+		assertEquals("ディストリビューター番号（お客様番号）または暗証番号が、正しく入力されておりません。\n(JWS5336E)",
+				closeAlertAndGetItsText());
+	}
 
-  @Test
-  public void testD01004010() throws Exception {
-    driver.get(baseUrl + "/aurora/source/ts/uc_common/view/WA-C-01-F012.html");
-    driver.findElement(By.cssSelector("a.naviOpen.naviOpenBtn > div > span")).click();
-    driver.findElement(By.id("aMenuLogin")).click();
-    driver.findElement(By.linkText("パスワードを忘れた方")).click();
-    driver.findElement(By.id("txtIboNumber")).clear();
-    driver.findElement(By.id("txtIboNumber")).sendKeys("5243017");
-    driver.findElement(By.id("txtPin")).clear();
-    driver.findElement(By.id("txtPin")).sendKeys("1061");
-    driver.findElement(By.cssSelector("#btnSetPassword > div")).click();
-    assertEquals("ディストリビューター番号（お客様番号）または暗証番号が、正しく入力されておりません。 (JWS5336E)", closeAlertAndGetItsText());
-  }
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alert.getText();
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 }
