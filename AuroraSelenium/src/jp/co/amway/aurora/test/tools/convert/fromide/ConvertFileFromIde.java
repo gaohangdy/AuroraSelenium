84342842c053ad389cd68e439f5f2d2bfe46d81c
gaohangdy <gaohangdy@gmail.com>
2013-03-19 05:57:21 -0300

    Tool for auroraSelenium Create testcase doc

---
+++ AuroraSelenium/src/jp/co/amway/aurora/test/tools/convert/fromide/ConvertFileFromIde.java
@@ -0,0 +1,13 @@
+package jp.co.amway.aurora.test.tools.convert.fromide;
+
+public class ConvertFileFromIde {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
